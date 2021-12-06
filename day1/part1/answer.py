b8eefdb256c387a23343ab4cce8f7ed4fe32201f
Sam <jto.sam@gmail.com>
2021-12-06 22:28:47 +0000

    d1p1 Use full input

--- day1/part1/answer.py
+++ day1/part1/answer.py
@@ -1,8 +1,10 @@
-input_data = [
-    199, 200, 208, 210, 200, 
-    207, 240, 269, 260, 263,
-]
+from pathlib import Path
 
+def get_input():
+    input = Path(__file__).with_name('input.txt')
+    return list(map(int, input.open('r').readlines()))
+
+input_data = get_input()
 count = 0
 for i in range(1, len(input_data)):
    if input_data[i] > input_data[i-1]:
@@ -11,3 +13,4 @@ for i in range(1, len(input_data)):
 print('How many measurements are larger than the previous measurement?')
 print(count)
 
+# ANSWER: 1832
